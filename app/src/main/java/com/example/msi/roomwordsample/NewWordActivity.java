4db4a18f5ffada5df1192f7d02f3190ad5f4e34f
parshin <ilyaiparshin@gmail.com>
2018-06-19 20:30:00 +0500

    11  chapter

---
+++ app/src/main/java/com/example/msi/roomwordsample/NewWordActivity.java
@@ -0,0 +1,12 @@
+package com.example.msi.roomwordsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_new_word extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
